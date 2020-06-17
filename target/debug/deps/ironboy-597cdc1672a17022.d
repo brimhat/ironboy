a8352c222bed054f7df63d6b7676591d727572cd
Christian54090 <christian.sanders@live.com>
2020-06-16 22:53:38 -0700

    Remove target folder

--- target/debug/deps/ironboy-597cdc1672a17022.d
+++
@@ -1,8 +0,0 @@
-/home/csanders/projects/ironboy/target/debug/deps/ironboy-597cdc1672a17022: src/main.rs src/mmu.rs src/registers.rs src/cpu.rs
-
-/home/csanders/projects/ironboy/target/debug/deps/ironboy-597cdc1672a17022.d: src/main.rs src/mmu.rs src/registers.rs src/cpu.rs
-
-src/main.rs:
-src/mmu.rs:
-src/registers.rs:
-src/cpu.rs:
